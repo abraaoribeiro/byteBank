285a93b23b456ad9fe5f5a2bf2c432ca2ccfbbb2
abraao.ribeiro <abraaohdg@gmail.com>
2021-05-10 16:24:49 -0300

    Carregando a lista de transferências na tela

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
@@ -9,10 +13,29 @@ class LoggingInterceptor implements InterceptorContract {
 
   @override
   Future<ResponseData> interceptResponse({ResponseData data}) async {
-      return data;
+    return data;
   }
 }
-void findAll() async{
-  HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
- final Response response = await get('http://192.168.1.7:8080/transactions');
+
+Future<List<Transaction>> findAll() async {
+  final HttpClientWithInterceptor client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+
+  final Response response = await get('http://192.168.1.7:8080/transactions');
+  final List<dynamic> decodeJson = jsonDecode(response.body);
+  final List<Transaction> transactions = [];
+
+  for (Map<String, dynamic> element in decodeJson) {
+    final Map<String, dynamic> contactJson = element['contact'];
+    final Transaction transaction = Transaction(
+      element['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transactions.add(transaction);
+  }
+
+  return transactions;
 }
--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,40 +1,70 @@
+import 'package:bytebank/componentes/progress.dart';
+import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = [];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Transactions'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+      body: FutureBuilder<List<Transaction>>(
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+             return  Progress();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              return ListTransaction(transactions: snapshot.data);
+              break;
+          }
+          return Text("Unknown error");
         },
-        itemCount: transactions.length,
       ),
     );
   }
 }
 
+class ListTransaction extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  const ListTransaction({
+    Key key,
+    this.transactions,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final Transaction transaction = transactions[index];
+        return Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text(
+              transaction.value.toString(),
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              transaction.contact.accountNumber.toString(),
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        );
+      },
+      itemCount: transactions.length,
+    );
+  }
+}

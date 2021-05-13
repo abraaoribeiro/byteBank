e53d705404f580ead7e896c2f1beaed62745e93a
abraao.ribeiro <abraaohdg@gmail.com>
2021-05-13 13:17:27 -0300

    Refatoração do codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:bytebank/http/webclient.dart';
-import 'package:bytebank/models/contact.dart';
-import 'package:bytebank/models/transaction.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(ByteBankApp());
-  save(Transaction(200.0, Contact(0, 'Abraao', 2000))).then((value) => print(value));
 }
 class ByteBankApp extends StatelessWidget {
   @override
